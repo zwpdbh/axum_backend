be4f60555d64ac308d941a52b07d50beee5d1c5e
zhaowei <zhaowei@microsoft.com>
2024-06-06 15:36:28 +0800

    <migration done

--- seaorm_demo/src/query/mod.rs
+++ seaorm_demo/src/query/mod.rs
@@ -1,5 +1,39 @@
+use async_graphql::ComplexObject;
+use async_graphql::Enum;
+use async_graphql::SimpleObject;
 use async_graphql::{Context, Object, Schema};
 use async_graphql::{EmptyMutation, EmptySubscription};
+
+#[derive(Debug, Enum, PartialEq, Eq, Clone, Copy)]
+
+enum Role {
+    Admin,
+    User,
+}
+
+impl Default for Role {
+    fn default() -> Self {
+        Self::User
+    }
+}
+
+// Define a complex object
+#[derive(Debug, SimpleObject, Default)]
+#[graphql(complex)]
+struct User {
+    role: Role,
+    username: String,
+    email: String,
+    address: String,
+    age: i32,
+}
+
+#[ComplexObject]
+impl User {
+    async fn users(&self) -> String {
+        format!("{:?}", self)
+    }
+}
 
 pub struct Query;
 
