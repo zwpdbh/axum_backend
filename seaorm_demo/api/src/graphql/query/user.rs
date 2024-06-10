46990020d152dbd5b671ec72cb74db4f783936bc
wdmbeat <danmei811@icloud.com>
2024-06-10 16:04:13 +0800

    I could get users now

--- seaorm_demo/api/src/graphql/query/user.rs
+++ seaorm_demo/api/src/graphql/query/user.rs
@@ -10,7 +10,7 @@ pub struct UserQuery;
 
 #[Object]
 impl UserQuery {
-    async fn get_note_by_id(&self, ctx: &Context<'_>, id: i32) -> Result<Option<user::Model>> {
+    async fn get_user_by_id(&self, ctx: &Context<'_>, id: i32) -> Result<Option<user::Model>> {
         let db = ctx.data::<Database>().unwrap();
         let conn = db.get_connection();
 
@@ -18,6 +18,16 @@ impl UserQuery {
             .await
             .map_err(|e| e.to_string())?)
     }
+
+    async fn get_users(&self, ctx: &Context<'_>) -> Result<Vec<user::Model>> {
+        let db = ctx.data::<Database>().unwrap();
+        let conn = db.get_connection();
+
+        Ok(Query::get_all_users(conn)
+            .await
+            .map_err(|e| e.to_string())?)
+    }
+
     async fn hello(&self, _ctx: &Context<'_>) -> &'static str {
         "Hello World"
     }
--- seaorm_demo/service/src/query.rs
+++ seaorm_demo/service/src/query.rs
@@ -8,4 +8,8 @@ impl Query {
     pub async fn find_user_by_id(db: &DbConn, id: i32) -> Result<Option<user::Model>, DbErr> {
         User::find_by_id(id).one(db).await
     }
+
+    pub async fn get_all_users(db: &DbConn) -> Result<Vec<user::Model>, DbErr> {
+        User::find().all(db).await
+    }
 }
