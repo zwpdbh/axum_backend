e3514bf2f306dcd6e67389fc2bb433f10da1cf79
wdmbeat <danmei811@icloud.com>
2024-06-10 17:21:40 +0800

    added mutation, build without error, to test

--- seaorm_demo/api/src/graphql/schema.rs
+++ seaorm_demo/api/src/graphql/schema.rs
@@ -1,13 +1,15 @@
-use async_graphql::EmptyMutation;
 use async_graphql::EmptySubscription;
 use async_graphql::Schema;
 use entity::async_graphql;
 
-use crate::db::Database;
-use crate::graphql::query::Query;
+use crate::{
+    db::Database,
+    graphql::{mutation::Mutation, query::Query},
+};
+
 use migration::{Migrator, MigratorTrait};
 
-pub type AppSchema = Schema<Query, EmptyMutation, EmptySubscription>;
+pub type AppSchema = Schema<Query, Mutation, EmptySubscription>;
 
 /// Builds the GraphQL Schema, attaching the Database to the context
 pub async fn build_schema() -> AppSchema {
@@ -15,7 +17,7 @@ pub async fn build_schema() -> AppSchema {
 
     Migrator::up(db.get_connection(), None).await.unwrap();
 
-    Schema::build(Query::default(), EmptyMutation, EmptySubscription)
+    Schema::build(Query::default(), Mutation::default(), EmptySubscription)
         .data(db)
         .finish()
 }
